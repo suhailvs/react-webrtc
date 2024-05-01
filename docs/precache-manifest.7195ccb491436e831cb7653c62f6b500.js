888d8d9bf12a719e33a0e69ca7a83cdc152c4c4d
suhailvs <suhailvs@gmail.com>
2024-05-01 17:19:44 +0530

    added docs

---
+++ docs/precache-manifest.7195ccb491436e831cb7653c62f6b500.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7999fef80376cbacd4b",
+    "url": "/static/css/main.3ba9fb91.chunk.css"
+  },
+  {
+    "revision": "c7999fef80376cbacd4b",
+    "url": "/static/js/main.3a402908.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c85686cd8021960d19e5",
+    "url": "/static/js/2.694953f2.chunk.js"
+  },
+  {
+    "revision": "e9a1cd9b238d2840adbfc105a3c46b0d",
+    "url": "/index.html"
+  }
+];
