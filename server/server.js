8f5986dabcec7b404f245793fd89a5e0d70837e3
suhailvs <suhailvs@gmail.com>
2024-05-01 18:27:10 +0530

    updated socket server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 var app = require('express')();
-var server = require('http').Server(app);
+var fs = require('fs');
+var options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+  };
+
+var server = require('https').Server(options,app);
 var io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 80);
+server.listen(process.env.PORT || 8080);
+
+
+
+
+// // Create an HTTP service.
+// http.createServer(app).listen(80);
+// // Create an HTTPS service identical to the HTTP service.
+// https.createServer(options, app).listen(443);
+
 
 io.on('connection', function (socket) {
     socket.on('join', function (data) {
